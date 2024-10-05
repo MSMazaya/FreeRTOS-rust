3ec16e39fe908b40adb1e9920a8fe284ceebfa22
Muhammad Sulthan Mazaya <73002733+MSMazaya@users.noreply.github.com>
2024-10-05 18:36:13 -0400

    remove vAssertCalled

--- freertos-rust/src/hooks.rs
+++ freertos-rust/src/hooks.rs
@@ -24,18 +24,4 @@ pub static mut FREERTOS_HOOKS: FreeRtosHooks = FreeRtosHooks { on_assert: || {} 
 #[allow(unused_doc_comments)]
 #[no_mangle]
 pub extern "C" fn vAssertCalled(file_name_ptr: FreeRtosCharPtr, line: FreeRtosUBaseType) {
-    let file_name: String;
-    unsafe {
-        file_name = str_from_c_string(file_name_ptr).unwrap();
-    }
-
-    unsafe {
-        FREERTOS_HOOKS.do_on_assert();
-    }
-
-    // we can't print without std yet.
-    // TODO: make the macro work for debug UART? Or use Panic here?
-    // println!("ASSERT: {} {}", line, file_name);
-    panic!("FreeRTOS ASSERT: {}:{}", file_name, line);
-    //loop {}
 }
